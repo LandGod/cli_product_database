69593c9030d7a0b62a5725ea3b32e80092dfeb5d
Dan Gold <tentativechaos@gmail.com>
2019-09-07 17:14:07 -0700

    Finished printAllProducts section. Created helper functions thenBackToMenu, for returning a user back to the main menu after they hit enter, and parseTable, for converting sql data so that it looks slightly better in console.table.

--- customer_interface.js
+++ customer_interface.js
@@ -1,16 +1,11 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+// Standard connection for local database
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "",
     database: "bamazon"
 });
@@ -18,6 +13,8 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+
+    // Start the rest of the program until this asynchronous action has been completed
     mainPrompt();
 });
 
@@ -26,6 +23,7 @@ function mainPrompt() {
         {
             name: 'initialChoice',
             type: 'list',
+            message: 'Hello! What would you like to do?',
             choices: [
                 'View All Available Merchandise',
                 'View Products by Department',
@@ -35,9 +33,9 @@ function mainPrompt() {
             ]
         }
     ]).then(answers => {
-        switch(answers.initialChoice) {
+        switch (answers.initialChoice) {
             case 'View All Available Merchandise':
-                // TODO:
+                printAllProducts();
                 break;
             case 'View Products by Department':
                 // TODO:
@@ -45,17 +43,61 @@ function mainPrompt() {
             case 'Search by Product Name':
                 // TODO:
                 break;
-            case 'Purchase Product by ID':
+            case 'Purchase Product by Index Number':
                 // TODO:
                 break;
             case 'Exit Store':
-            default: 
+            default:
                 console.log('Thank you! Come again!');
                 process.exit();
         };
     });
 };
 
+// This function takes a list of objects returned from a mySQL query and converts them
+// into a single object indexed by the product number, for better display in a table
+function parseTable(rawData) {
+    let tableObj = {}
+    for (let i = 0; i < rawData.length; i++) {
+        tableObj[rawData[i].item_id] = {
+            'Product Name': rawData[i].product_name,
+            'Department': rawData[i].department_name,
+            'Price': `$${rawData[i].price}`
+        }
+    }
+
+    return tableObj;
+};
+
+// Prompts the user to confirm that they wish to continue, then return to main menu
+function thenBackToMenu() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'continue',
+            message: "Press [Enter] to continue"
+        }
+    ]).then(x => {
+        mainPrompt();
+    })
+};
+
+
 function printAllProducts() {
-    //TODO:
+    connection.query('SELECT * FROM products WHERE stock > 0',
+        function (err, res) {
+            if (err) { throw (err) };
+            if (res) {
+                console.table(parseTable(res));
+            }
+
+            else { console.log(`Error: Database Unreachable`) };
+
+
+            // When done, after user hits enter, return back to first prompt
+            
+            thenBackToMenu();
+
+        })
 };
